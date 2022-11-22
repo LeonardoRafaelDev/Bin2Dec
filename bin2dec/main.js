b622c99613ad554e434a296ac3a31c606a813f1f
LeonardoRafaelDev <leonardorafaeldev@gmail.com>
2022-11-22 18:15:56 -0300

    make html and css styledc

---
+++ bin2dec/main.js
@@ -0,0 +1,7 @@
+let inputBinary = document.getElementById("binary");
+let convert = document.getElementById("convert");
+let alerta = document.getElementById("alerta");
+
+//criar validaçao de 01 pegar regex
+
+// criar funçao para converter
